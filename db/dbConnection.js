845fb6673409ad3e8c7441a75a130b4263081cb8
abenaesha <abdulraofbenaesha@yahoo.co.uk>
2021-03-16 20:14:17 +0000

    update connection.js

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,16 +1,34 @@
+// const knex = require('knex');
+// const dbConfig = require('../knexfile');
+
+// const dbConnection = ENV === 'production'
+//   ? {
+//     client: 'pg',
+//     connection: {
+//       connectionString: process.env.DATABASE_URL,
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     },
+//   }
+//   : knex(dbConfig);
+
+// module.exports = dbConnection;
+
+const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
-const dbConfig = require('../knexfile');
 
-const dbConnection = ENV === 'production'
-  ? {
-    client: 'pg',
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  }
-  : knex(dbConfig);
+const dbConfig =
+  ENV === 'production'
+    ? { 
+        client: 'pg', 
+        connection: { 
+          connectionString: process.env.DATABASE_URL,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }, 
+      }
+    : require('../knexfile');
 
-module.exports = dbConnection;
+module.exports = knex(dbConfig);
